8652e0ada2417265b660b071ffe428adbbc0f605
DreasWeiss <dreasweiss@gmail.com>
2022-12-26 22:42:53 +0300

    added body-parser, added functional to serve some data

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -24,7 +30,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  // let header = JSON.stringify(req.headers);
+  let header = req.headers;
+  let taskInfo = {
+    "ipaddress": header.referer,
+    "language" : header["accept-language"],
+    "software" : header["user-agent"]
+  };
+  return res.end(JSON.stringify(taskInfo)); 
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
